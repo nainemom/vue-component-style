f5b836e0fa57f6fc4622d35e620f0609b0b9c1a7
Amir Momenian <nainemom@gmail.com>
2019-11-30 19:34:47 +0330

    chore: remove options key

--- nuxt/index.js
+++ nuxt/index.js
@@ -3,7 +3,6 @@ import path from 'path';
 export default function VueComponentStyle () {
   // Write your code here
   this.addPlugin({
-    src: path.resolve(__dirname, 'plugin.js'),
-    options: {}
+    src: path.resolve(__dirname, 'plugin.js')
   })
 }
