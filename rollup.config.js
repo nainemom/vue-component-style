3b1b5be9429e99e01ebf7512540b1a311a97c628
Amir Momenian <nainemom@gmail.com>
2019-12-01 00:03:33 +0330

    feat: minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,25 @@
-function generateConfig(format) {
+import { terser } from 'rollup-plugin-terser'
+
+function generateConfig(format, minify) {
   const nameFormat = format === 'umd' ? '' : `.${format}`
+  const nameMinfy = minify && format === 'umd' ? '.min' : ''
+  const plugins = []
+  if (minify) {
+    plugins.push(terser())
+  }
   return {
     input: 'src/index.js',
     output: {
-      file: `dist/vue-component-style${nameFormat}.js`,
+      file: `dist/vue-component-style${nameFormat}${nameMinfy}.js`,
       name: 'VueComponentStyle',
       format
-    }
+    },
+    plugins
   }
 }
 module.exports = [
-  generateConfig('umd'),
-  generateConfig('cjs'),
-  generateConfig('esm'),
+  generateConfig('umd', true),
+  generateConfig('umd', false),
+  generateConfig('cjs', true),
+  generateConfig('esm', true),
 ];
