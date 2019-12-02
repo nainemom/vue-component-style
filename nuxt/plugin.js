506a710714d6f43d881ea121ada9ec4407a620e2
nainemom <nainemom@gmail.com>
2019-12-02 20:00:01 +0330

    chore: lint

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import VueComponentStyle from 'vue-component-style';
+import Vue from 'vue'; // eslint-disable-line import/no-extraneous-dependencies
+import VueComponentStyle from 'vue-component-style'; // eslint-disable-line import/no-extraneous-dependencies
 
 export default ({ app: { head } }) => {
   Vue.use(VueComponentStyle, {
