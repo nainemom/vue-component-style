1fb2d19cbcb0ee2a24d0c309a1e8d877c5e5527a
nainemom <nainemom@gmail.com>
2019-12-04 20:09:42 +0330

    chore: cleanup

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,13 +1,6 @@
 import Vue from 'vue'; // eslint-disable-line import/no-extraneous-dependencies
 import VueComponentStyle from 'vue-component-style'; // eslint-disable-line import/no-extraneous-dependencies
 
-export default ({ app: { head } }) => {
-  console.log('plugin called!', {
-    headObject: typeof document === 'undefined' ? head : undefined,
-    documentObject: typeof document !== 'undefined' ? document : undefined,
-  });
-  Vue.use(VueComponentStyle, {
-    headObject: typeof document === 'undefined' ? head : undefined,
-    documentObject: typeof document !== 'undefined' ? document : undefined,
-  });
+export default () => {
+  Vue.use(VueComponentStyle);
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import mixin from './mixin';
 
 export default {
-  install(Vue, userOptions = {}) {
-    const options = Object.assign(userOptions);
-    if (!userOptions.documentObject && typeof document !== 'undefined') {
-      options.documentObject = document;
-    }
-    Vue.mixin(mixin(options));
+  install(Vue) {
+    Vue.mixin(mixin);
   },
 };
