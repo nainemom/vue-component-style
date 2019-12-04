8a316bea92d1e3bb9dbd89829ecac9fd1b992f04
Amir Momenian <nainemom@gmail.com>
2019-12-05 01:00:16 +0330

    add: _ssrAppObject to Vue prototype

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'; // eslint-disable-line import/no-extraneous-dependencies
 import VueComponentStyle from 'vue-component-style'; // eslint-disable-line import/no-extraneous-dependencies
 
-export default () => {
+export default ({ app: { head: { style } } }) => {
+  // eslint-disable-next-line no-underscore-dangle
+  Vue.prototype._ssrAppObject = {
+    head: {
+      style,
+    },
+  };
   Vue.use(VueComponentStyle);
 };
