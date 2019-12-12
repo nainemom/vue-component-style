9490c36a3119b6dcdc31eb01db55b65f715c0228
Amir Momenian <nainemom@gmail.com>
2019-12-12 20:36:02 +0330

    fix: update rebuild actions

--- src/mixin.js
+++ src/mixin.js
@@ -7,53 +7,44 @@ export default {
   created() {
     this.$calcStyle();
   },
+  beforeUpdate() {
+    this.$calcStyle();
+  },
   methods: {
     $calcStyle() {
-      const documentObject = typeof document !== 'undefined' ? document : undefined;
-      const ssrAppObject = this._ssrAppObject;
-      const lastStyleId = this.$lastStyleId;
       const propValue = this.$options.style;
-      // delete old stylesheet if found
-      if (typeOf(lastStyleId) !== 'Undefined') {
-        deleteStylesheet(lastStyleId, documentObject, ssrAppObject);
-      }
-
-      if (typeOf(propValue) === 'Function') {
-        const styleId = hashCode(propValue.toString(), this.$data, JSON.stringify(this.$props));
-        const helper = Helper(styleId);
-        const value = propValue.call(this, helper);
-        if (typeOf(value) !== 'Array') {
-          // style is passed and it's function, but return value is not object
-          makeError('\'style\' function should returns Array!');
+      if (typeOf(propValue) !== 'Undefined') {
+        const documentObject = typeof document !== 'undefined' ? document : undefined;
+        const ssrAppObject = this._ssrAppObject;
+        const lastStyleId = this.$lastStyleId;
+        // delete old stylesheet if found
+        if (typeOf(lastStyleId) !== 'Undefined') {
+          deleteStylesheet(lastStyleId, documentObject, ssrAppObject);
         }
-        const css = value.join('');
-        injectStylesheet(styleId, css, documentObject, ssrAppObject);
-        this.$style = helper.maps;
-        this.$lastStyleId = styleId;
-        this.$forceUpdate();
-        this.$nextTick(() => { // wait until component-style new class-names applied to component
-          setTimeout(() => { // wait until component-style updates global style tag
-            this.$emit('styleChange', this.$style);
+
+        if (typeOf(propValue) === 'Function') {
+          const styleId = hashCode(propValue.toString(), this.$data, JSON.stringify(this.$props));
+          const helper = Helper(styleId);
+          const value = propValue.call(this, helper);
+          if (typeOf(value) !== 'Array') {
+            // style is passed and it's function, but return value is not object
+            makeError('\'style\' function should returns Array!');
+          }
+          const css = value.join('');
+          injectStylesheet(styleId, css, documentObject, ssrAppObject);
+          this.$style = helper.maps;
+          this.$lastStyleId = styleId;
+          this.$forceUpdate();
+          this.$nextTick(() => { // wait until component-style new class-names applied to component
+            setTimeout(() => { // wait until component-style updates global style tag
+              this.$emit('styleChange', this.$style);
+            });
           });
-        });
-      } else if (typeOf(propValue) !== 'Undefined') {
-        // style is passed, but with wrong value
-        makeError('\'style\' should be function!');
+        } else {
+          // style is passed, but with wrong value
+          makeError('\'style\' should be function!');
+        }
       }
-    },
-  },
-  watch: {
-    $data: {
-      deep: true,
-      handler() {
-        this.$calcStyle();
-      },
-    },
-    $props: {
-      deep: true,
-      handler() {
-        this.$calcStyle();
-      },
     },
   },
 };
