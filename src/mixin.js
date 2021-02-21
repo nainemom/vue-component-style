1f06fba0ba652cf2bcdf18dd89a795dd83f89f3b
Amir Momenian <nainemom@gmail.com>
2021-02-21 23:30:56 +0330

    remove forceUpdate after style updates

--- src/mixin.js
+++ src/mixin.js
@@ -32,7 +32,6 @@ export default {
           injectStylesheet(styleId, css, documentObject, ssrAppObject);
           this.$style = helper.maps;
           this.$lastStyleId = styleId;
-          this.$forceUpdate();
           this.$nextTick(() => { // wait until component-style new class-names applied to component
             setTimeout(() => { // wait until component-style updates global style tag
               this.$emit('styleChange', this.$style);
